3ed5f18df49ac4c7ea0c7190cd5b4e975a2d5059
zhaoy992 <zhaoy992@newschool.edu>
2021-03-26 20:50:21 +0800

    kk

---
+++ visual tour/js.js
@@ -0,0 +1,53 @@
+// JavaScript Document
+
+
+
+function bigImg(x) {
+  x.style.height = "300px";
+  
+}
+
+function normalImg(x) {
+  x.style.height = "400px";
+  
+}
+
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "8-1002, Tiyunanlu 95, BeimeiN1, Taiyuan, Shanxi, China";
+}
+function click3(){
+    document.getElementById("myDIV").innerHTML = "Are you ready?";
+    document.getElementById("a33").style.transform= 'rotate('+90+'deg)';
+
+}
+function click4(){
+    document.getElementById("a44").innerHTML = "I love it!";
+    document.getElementById("a44").style.background = "hotpink";
+
+}
+function click5(){
+    document.getElementById("a55").style.height = "550px";
+    document.getElementById("a55").style.width = "550px";
+}
+function click6(){
+    document.getElementById("a66").src="IMG_1183.jpg";
+
+}
+function click7(){
+    document.getElementById("a7").style.transform= 'rotate('+180+'deg)';
+
+}
+function click8(){
+    document.getElementsByClassName("a88")[0].innerHTML = "These cakes are really delicious";
+    document.getElementsByClassName("a8")[0].style.background = "pink";
+
+}
+
+function click9(){
+    document.getElementsByClassName("a9")[0].style.height = "700px";
+}
+
+function click10(){
+    document.getElementsByClassName("a5")[0].style.color = "deeppink";
+}
