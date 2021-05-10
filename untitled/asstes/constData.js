2cd9a52f0179e63b969f9ecee343d2a3c6a4a63e
zhaoy992 <zhaoy992@newschool.edu>
2021-05-10 23:49:01 +0800

    lllll

---
+++ untitled/asstes/constData.js
@@ -0,0 +1,242 @@
+
+
+const publicHref = 'file:///Users/zhaoyufan/Desktop/homePageFFF/views/homePageList.html'
+const switchTime = 6
+const imgChangeTime = 1
+const constData = {
+    'linkJson': [
+        {
+            'key':'STUDIOPROJECTS',
+            'list': [
+                {
+                    'imgUrl01': '../homepage/homepage/studio projects/p1/截屏2021-05-05 下午11.37.13.png',
+                    'imgUrl02': '../homepage/homepage/studio projects/p1/截屏2021-05-05 下午11.37.29.png',
+                    'url': 'https://zhaoy992.github.io/project%201/Untitled-12.html',
+                    "font":"Project 1",
+                    "checked":1
+                },{
+                    'imgUrl01': '../homepage/homepage/studio projects/p2.1/截屏2021-05-05 下午11.38.36.png',
+                    'imgUrl02': '../homepage/homepage/studio projects/p2.1/截屏2021-05-05 下午11.38.36.png',
+                    'url': 'https://docs.google.com/document/d/1ejMkkcfXJt0tJKWysQaNerlUsMbWM4XsK1EBHIYa71Y/edit',
+                    "font":"Project 2: Part1",
+                    "checked":1
+                },{
+                    'imgUrl01': '../homepage/homepage/studio projects/p2.2/截屏2021-05-05 下午11.39.04.png',
+                    'imgUrl02': '../homepage/homepage/studio projects/p2.2/截屏2021-05-05 下午11.39.15.png',
+                    'url': 'https://docs.google.com/document/d/1wOpIShD3CpDeWt9vqUUs-7BeCen7LurQMjtkZDbiuxU/edit?usp=sharing',
+                    "font":"Project 2: Part2",
+                    "checked":1
+                },{
+                    'imgUrl01': '../homepage/homepage/studio projects/p2.3/截屏2021-05-05 下午11.39.36.png',
+                    'imgUrl02': '../homepage/homepage/studio projects/p2.3/截屏2021-05-05 下午11.39.48.png',
+                    'url': 'https://zhaoy992.github.io/star/star.html',
+                    "font":"Project 2: Part3",
+                    "checked":1
+                },{
+                    'imgUrl01': '../homepage/homepage/studio projects/p2.4/截屏2021-05-05 下午11.40.15.png',
+                    'imgUrl02': '../homepage/homepage/studio projects/p2.4/截屏2021-05-05 下午11.40.35.png',
+                    'url': 'https://zhaoy992.github.io/star/star1%202.html',
+                    "font":"Project 2: Part4(1)",
+                    "checked":1
+                },{
+                    'imgUrl01': '../homepage/homepage/studio projects/p2.5/截屏2021-05-05 下午11.41.10.png',
+                    'imgUrl02': '../homepage/homepage/studio projects/p2.5/截屏2021-05-05 下午11.41.24.png',
+                    'url': 'https://zhaoy992.github.io/star/star1.html',
+                    "font":"Project 2: Part4(2)",
+                    "checked":1
+                },{
+                    'imgUrl01': '../homepage/homepage/studio projects/p2 final/截屏2021-05-05 下午11.41.47.png',
+                    'imgUrl02': '../homepage/homepage/studio projects/p2 final/截屏2021-05-05 下午11.42.03.png',
+                    'url': 'https://zhaoy992.github.io/star/Untitled-2.html',
+                    "font":"Project 2: Final",
+                    "checked":1
+                },{
+                    'imgUrl01': '../homepage/homepage/studio projects/p3.5/截屏2021-05-05 下午11.42.25.png',
+                    'imgUrl02': '../homepage/homepage/studio projects/p3.5/截屏2021-05-05 下午11.42.25.png',
+                    'url': 'https://zhaoy992.github.io/p3/test.html',
+                    "font":"Project 3: Part5",
+                    "checked":1
+                },{
+                    'imgUrl01': '../homepage/homepage/studio projects/p3.6/截屏2021-05-05 下午11.42.50.png',
+                    'imgUrl02': '../homepage/homepage/studio projects/p3.6/截屏2021-05-05 下午11.43.00.png',
+                    'url': 'https://zhaoy992.github.io/p3/Untitled-2.html',
+                    "font":"Project 3: Part6",
+                    "checked":1
+                },{
+                    'imgUrl01': '../homepage/homepage/studio projects/p3.7/截屏2021-05-05 下午11.43.33.png',
+                    'imgUrl02': '../homepage/homepage/studio projects/p3.7/截屏2021-05-05 下午11.43.33.png',
+                    'url': 'https://zhaoy992.github.io/p3/test.html',
+                    "font":"Project 3: Part7",
+                    "checked":1
+                },{
+                    'imgUrl01': '../homepage/homepage/studio projects/p3.7/截屏2021-05-05 下午11.43.33.png',
+                    'imgUrl02': '../homepage/homepage/studio projects/p3.7/截屏2021-05-05 下午11.43.33.png',
+                    'url': 'https://zhaoy992.github.io/p3/test.html',
+                    "font":"Project 3: Part8",
+                    "checked":1
+                },{
+                    'imgUrl01': '../homepage/homepage/studio projects/p3.8/截屏2021-05-05 下午11.44.09.png',
+                    'imgUrl02': '../homepage/homepage/studio projects/p3.8/截屏2021-05-05 下午11.44.29.png',
+                    'url': 'https://zhaoy992.github.io/p3/mouse.html',
+                    "font":"Project 3: Part9",
+                    "checked":1
+                },{
+                    'imgUrl01': '../homepage/homepage/studio projects/p3 final/截屏2021-05-05 下午11.45.01.png',
+                    'imgUrl02': '../homepage/homepage/studio projects/p3 final/截屏2021-05-05 下午11.45.01.png',
+                    'url': 'https://zhaoy992.github.io/p3/mouse.html',
+                    "font":"Project 3: Final",
+                    "checked":1
+                },{
+                    'imgUrl01': '../homepage/homepage/studio projects/p4.3/截屏2021-05-05 下午11.32.50.png',
+                    'imgUrl02': '../homepage/homepage/studio projects/p4.3/截屏2021-05-05 下午11.32.50.png',
+                    'url': 'https://zhaoy992.github.io/emoji(2)/hello.html',
+                    "font":"Project 4: Part3",
+                    "checked":1
+                },{
+                    'imgUrl01': '../homepage/homepage/studio projects/p4.4/截屏2021-05-05 下午11.34.11.png',
+                    'imgUrl02': '../homepage/homepage/studio projects/p4.4/截屏2021-05-05 下午11.34.33.png',
+                    'url': 'https://zhaoy992.github.io/emoji(1)/home.html',
+                    "font":"Project 4: Part4",
+                    "checked":1
+                },{
+                    'imgUrl01': '../homepage/homepage/studio projects/p4 final/截屏2021-05-05 下午11.35.21.png',
+                    'imgUrl02': '../homepage/homepage/studio projects/p4 final/截屏2021-05-05 下午11.35.36.png',
+                    'url': 'https://zhaoy992.github.io/emoji(2)/home.html',
+                    "font":"Project 4: Final",
+                    "checked":1
+                }
+            ]
+        },{
+            'key':'STUDIOWORKSHOPS',
+            'list': [
+                {
+                    'imgUrl01': '../homepage/homepage/studio workshop/canlender/截屏2021-05-05 下午11.19.26.png',
+                    'imgUrl02': '../homepage/homepage/studio workshop/canlender/截屏2021-05-05 下午11.19.26.png',
+                    'url': 'https://zhaoy992.github.io/interview/online.html',
+                    "font":"Interview",
+                    "checked":1
+                },
+                {
+                    'imgUrl01': '../homepage/homepage/studio workshop/favicon/截屏2021-05-05 下午11.22.04.png',
+                    'imgUrl02': '../homepage/homepage/studio workshop/favicon/截屏2021-05-05 下午11.22.04.png',
+                    'url': 'https://zhaoy992.github.io/can.html',
+                    "font":"Canlender",
+                    "checked":1
+                },
+                {
+                    'imgUrl01': '../homepage/homepage/studio workshop/filter/截屏2021-05-05 下午11.21.24.png',
+                    'imgUrl02': '../homepage/homepage/studio workshop/filter/截屏2021-05-05 下午11.21.36.png',
+                    'url': 'https://zhaoy992.github.io/filter/Untitled-1%E7%9A%84%E5%89%AF%E6%9C%AC.html',
+                    "font":"Filter",
+                    "checked":1
+                },
+                {
+                    'imgUrl01': '../homepage/homepage/studio workshop/interview/截屏2021-05-05 下午11.18.50.png',
+                    'imgUrl02': '../homepage/homepage/studio workshop/interview/截屏2021-05-05 下午11.19.02.png',
+                    'url': 'https://zhaoy992.github.io/favicon/favicon.html',
+                    "font":"Favicon",
+                    "checked":1
+                },
+                {
+                    'imgUrl01': '../homepage/homepage/studio workshop/text/截屏2021-05-05 下午11.23.11.png',
+                    'imgUrl02': '../homepage/homepage/studio workshop/text/截屏2021-05-05 下午11.23.21.png',
+                    'url': 'https://zhaoy992.github.io/textgene.html',
+                    "font":"Text Generator",
+                    "checked":1
+                },
+                {
+                    'imgUrl01': '../homepage/homepage/studio workshop/web/截屏2021-05-05 下午11.22.29.png',
+                    'imgUrl02': '../homepage/homepage/studio workshop/web/截屏2021-05-05 下午11.22.50.png',
+                    'url': 'https://docs.google.com/document/d/1TnGLwhqzIpPEoOg_ViqKTF4m05gnoEGp2vklvHDE01Q/edit',
+                    "font":"Web Exercise",
+                    "checked":1
+                }
+            ]
+        },{
+            'key':'LABPROJECTS',
+            'list': [
+                {
+                    'imgUrl01': '../homepage/homepage/lab projects/1/截屏2021-05-05 下午11.06.17.png',
+                    'imgUrl02': '../homepage/homepage/lab projects/1/截屏2021-05-05 下午11.06.17.png',
+                    'url': 'https://zhaoy992.github.io/review/review.html',
+                    "font":"Assignment 1",
+                    "checked":1
+                }, {
+                    'imgUrl01': '../homepage/homepage/lab projects/2/截屏2021-05-05 下午11.08.06.png',
+                    'imgUrl02': '../homepage/homepage/lab projects/2/截屏2021-05-05 下午11.08.06.png',
+                    'url': 'https://zhaoy992.github.io/community%20agreement.html',
+                    "font":"Assignment 2",
+                    "checked":1
+                }, {
+                    'imgUrl01': '../homepage/homepage/lab projects/3/截屏2021-05-05 下午11.08.52.png',
+                    'imgUrl02': '../homepage/homepage/lab projects/3/截屏2021-05-05 下午11.09.14.png',
+                    'url': 'https://core-interaction-spring-2021.rejuvenatethe.net/#home',
+                    "font":"Assignment 3",
+                    "checked":1
+                }, {
+                    'imgUrl01': '../homepage/homepage/lab projects/4/截屏2021-05-05 下午11.09.55.png',
+                    'imgUrl02': '../homepage/homepage/lab projects/4/截屏2021-05-05 下午11.10.21.png',
+                    'url': 'https://zhaoy992.github.io/visual%20tour/1.html',
+                    "font":"Assignment 4: Part1",
+                    "checked":1
+                },
+                {
+                    'imgUrl01': '../homepage/homepage/lab projects/5/截屏2021-05-05 下午11.10.52.png',
+                    'imgUrl02': '../homepage/homepage/lab projects/5/截屏2021-05-05 下午11.11.28.png',
+                    'url': 'https://zhaoy992.github.io/visual%20tour/1.html',
+                    "font":"Assignment 5: Part2",
+                    "checked":1
+                }, {
+                    'imgUrl01': '../homepage/homepage/lab projects/6/截屏2021-05-05 下午11.12.21.png',
+                    'imgUrl02': '../homepage/homepage/lab projects/6/截屏2021-05-05 下午11.12.42.png',
+                    'url': 'https://zhaoy992.github.io/visual%20tour/1.html',
+                    "font":"Assignment 6: Part3",
+                    "checked":1
+                }, {
+                    'imgUrl01': '../homepage/homepage/lab projects/7/截屏2021-05-05 下午11.13.40.png',
+                    'imgUrl02': '../homepage/homepage/lab projects/7/截屏2021-05-05 下午11.13.58.png',
+                    'url': 'https://zhaoy992.github.io/visual%20tour/1.html',
+                    "font":"Assignment 7: Part4",
+                    "checked":1
+                }, {
+                    'imgUrl01': '../homepage/homepage/lab projects/8/截屏2021-05-05 下午11.14.47.png',
+                    'imgUrl02': '../homepage/homepage/lab projects/8/截屏2021-05-05 下午11.15.15.png',
+                    'url': 'https://zhaoy992.github.io/set-the-airtable/index.html',
+                    "font":"Assignment 8: Part1",
+                    "checked":1
+                }, {
+                    'imgUrl01': '../homepage/homepage/lab projects/10/截屏2021-05-05 下午11.15.37.png',
+                    'imgUrl02': '../homepage/homepage/lab projects/10/截屏2021-05-05 下午11.15.49.png',
+                    'url': 'https://github.com/zhaoy992/zhaoy992.github.io/blob/master/star/part5.html',
+                    "font":"Assignment 10: Technical Reflection",
+                    "checked":1
+                }
+            ]
+        },{
+            'key':'LABWORKSHOPS',
+            'list': [
+                {
+                    'imgUrl01': '../homepage/homepage/lab workshops/agreement/截屏2021-05-05 下午11.16.53.png',
+                    'imgUrl02': '../homepage/homepage/lab workshops/agreement/截屏2021-05-05 下午11.16.53.png',
+                    'url': 'https://zhaoy992.github.io/class%20cookbook/index.html',
+                    "font":"Class Cookbook",
+                    "checked":1
+                },
+                {
+                    'imgUrl01': '../homepage/homepage/lab workshops/cookbook/截屏2021-05-05 下午11.16.19.png',
+                    'imgUrl02': '../homepage/homepage/lab workshops/cookbook/截屏2021-05-05 下午11.16.19.png',
+                    'url': 'https://zhaoy992.github.io/there-are-no/stupid-questions/or-stupid-answers/index.html',
+                    "font":"Community Agreement",
+                    "checked":1
+                },
+                {
+                    'imgUrl01': '../homepage/homepage/lab workshops/responsive/截屏2021-05-05 下午11.17.28.png',
+                    'imgUrl02': '../homepage/homepage/lab workshops/responsive/截屏2021-05-05 下午11.17.57.png',
+                    'url': 'https://zhaoy992.github.io/resposive.html',
+                    "font":"Responsive Page",
+                    "checked":1
+                }
+            ]
+        },
+    ]
+}
